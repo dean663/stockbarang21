56423c7988d5c2f7eec6e0d1c4bf29e447897f6b
dean663 <dean8745316@gmail.com>
2025-01-02 10:31:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<?php
+require 'function.php';
+require 'cek.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.php">Stock Barang</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Stock Barang
+                            </a>
+                            <a class="nav-link" href="masuk.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Barang Masuk
+                            </a>
+                            <a class="nav-link" href="keluar.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Barang Keluar
+                            </a>
+                            <a class="nav-link" href="logout.php">
+                                logout
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Stock Barang</h1>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                                    Tambah Barang
+                                </button>
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Id barang</th>
+                                            <th>nama barang</th>
+                                            <th>deskripsi</th>
+                                            <th>stock</th>
+                                            <th>aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $ambilsemuadatastock = mysqli_query($conn,"SELECT * FROM stock");
+                                        $i = 1;
+                                        while($data=mysqli_fetch_array($ambilsemuadatastock)){
+                                           
+                                            $namabarang = $data['namabarang'];
+                                            $deskripsi = $data['deskripsi'];
+                                            $stock = $data['stock'];    
+                                            $idb = $data['idbarang'];                                  
+                                        ?>
+                                   
+                                        <tr>
+                                            <td><?=$i++;?></td>
+                                            <td><?=$namabarang;?></td>
+                                            <td><?=$deskripsi;?></td>
+                                            <td><?=$stock;?></td>
+                                            <td>
+                                               <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#edit<?=$idb;?>">
+                                                Edit
+                                               </button>
+                                               <input type="hidden" name="idbarangyangmaudihapus" value="<?=$idb;?>">
+                                               <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete<?=$idb;?>">
+                                                Delete
+                                               </button>
+
+                                            </td>
+                                        </tr>
+                                            <div class="modal fade" id="edit<?=$idb;?>">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+
+                                                <!-- Modal Header -->
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title">Edit Barang</h4>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                                </div>
+
+                                                <!-- Modal body -->
+                                                <form method="post">
+                                                <div class="modal-body">
+                                                <input type="text" name="namabarang" value="<?=$namabarang;?>" class="form-control" required>
+                                                <br>
+                                                <br>
+                                                <input type="text" name="deskripsi" value="<?=$deskripsi;?>" class="form-control" required>
+                                                <br>
+                                                <br>
+                                                <input type="hidden" name="idb" value="<?=$idb;?>">
+                                                <button type="submit" class="btn btn-primary" name="updatebarang">Submit</button>
+                                                </div>
+                                                </form>
+                                                </div>
+                                            </div>
+                                            </div>
+
+                                            <div class="modal fade" id="delete<?=$idb;?>">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+
+                                                <!-- Modal Header -->
+                                                <div class="modal-header">
+                                                Apakah anda ingin menghapus <?=$namabarang;?>?
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                                </div>
+
+                                                <!-- Modal body -->
+                                                <form method="post">
+                                                <div class="modal-body">
+                                                <input type="hidden" name="idb" value="<?=$idb;?>">
+                                                <button type="submit" class="btn btn-primary" name="hapusbarang">Hapus</button>
+                                                </div>
+                                                </form>
+                                                </div>
+                                            </div>
+                                            </div>
+
+
+                                        <?php
+                                          };
+
+                                        ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+        <div class="modal fade" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+        <!-- Modal Header -->
+        <div class="modal-header">
+            <h4 class="modal-title">Tambah Barang</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+
+        <!-- Modal body -->
+        <form method="post">
+        <div class="modal-body">
+        <input type="text" name="namabarang" placeholder="namabarang" class="form-control" required>
+        <br>
+        <br>
+        <input type="text" name="deskripsi" placeholder="deskripsi" class="form-control" required>
+        <br>
+        <br>
+        <input type="number" name="stock" placeholder="stock" class="form-control" required>
+        <br>
+        <br>
+        <button type="submit" class="btn btn-primary" name="addnewbarang">Submit</button>
+        </div>
+        </form>
+        </div>
+    </div>
+    </div>
+</html>
